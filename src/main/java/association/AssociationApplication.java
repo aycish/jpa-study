1f0c498bbb9830143233da4b4214a2d596824880
unhee_jo <unhee_jo@tmax.co.kr>
2022-08-25 18:17:12 +0900

    feat : 양방향 맵핑 중간 커밋

--- src/main/java/association/AssociationApplication.java
+++ src/main/java/association/AssociationApplication.java
@@ -8,35 +8,41 @@ import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.EntityTransaction;
 import javax.persistence.Persistence;
+import java.util.List;
 
 public class AssociationApplication {
     public static void main(String[] args) {
         EntityManagerFactory emf = Persistence.createEntityManagerFactory("association");
         EntityManager em = emf.createEntityManager();
         EntityTransaction tx = em.getTransaction();
-        tx.begin();
+
 
         try{
+            tx.begin();
             Team team = new Team();
             team.setName("TeamA");
             em.persist(team);
 
             Mate mate = new Mate();
             mate.setUserName("member1");
-
+            // 정석적인 방법으로 객체에 연관관계를 직접 넣어준다. -> 보통 연관관계 편의 메서드를 정의한다.
             mate.setTeam(team);
+            team.getMates().add(mate);
             em.persist(mate);
 
-            /* DB에서 직접 값을 가져오고 싶은 경우
-             * em.flush();
-             * em.clear();
-             */
+            /* insert 쿼리가 발행되지 않았기 때문에, select 쿼리 또한 발행되지 않는다.
+            *  따라서 flush를 실행하여 insert가 완료된 후, select 쿼리를 보낼 수 있도록 한다.
+            * */
+            em.flush();
 
             Mate findMate = em.find(Mate.class, mate.getId());
             Team findTeam = findMate.getTeam();
-            System.out.println("findTeam = " + findTeam.getId());
+            List<Mate> findMates = findTeam.getMates();
+            System.out.println("team = " + findTeam.getName());
+            findMates.forEach((m)-> System.out.println("member = " + m.getUserName()));
 
             tx.commit();
+
         } catch (Exception e) {
             tx.rollback();
         } finally {
--- src/main/java/association/domain/Team.java
+++ src/main/java/association/domain/Team.java
@@ -1,16 +1,23 @@
 package association.domain;
 
 import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
 
 import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
 
-@Data
 @Table(name = "TEAM")
 @Entity
+@Data
 public class Team {
     @Id @GeneratedValue(strategy = GenerationType.AUTO)
     @Column(name = "TEAM_ID")
     private Long id;
 
     private String name;
+
+    @OneToMany(mappedBy = "team")
+    List<Mate> mates = new ArrayList<>();
 }
