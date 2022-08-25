22a57b3f97130c0c2d4d4210c6982a08d721e73b
unhee_jo <unhee_jo@tmax.co.kr>
2022-08-25 18:17:12 +0900

    feat : 양방향 맵핑 중간 커밋

--- src/main/java/association/domain/Mate.java
+++ src/main/java/association/domain/Mate.java
@@ -1,18 +1,23 @@
 package association.domain;
 
 import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
 
 import javax.persistence.*;
 
-@Data
+
 @Entity
 @Table(name = "MATE")
+@Data
 public class Mate {
 
     @Id @GeneratedValue(strategy = GenerationType.AUTO)
     @Column(name = "MATE_ID")
     private Long id;
 
+    @Column(name = "USERNAME")
+    private String userName;
     // 객체 지향적으로 설계하려면 하기 방법은 피해야한다.
     //@Column(name = "TEAM_ID")
     //private Long teamId;
@@ -20,7 +25,4 @@ public class Mate {
     @ManyToOne
     @JoinColumn(name = "TEAM_ID")
     private Team team;
-
-    @Column(name = "USERNAME")
-    private String userName;
 }
