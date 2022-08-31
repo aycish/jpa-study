0c3bebdf1b8fab2b9de4650f4320e80e328dc1d6
aycish <aycish07@gmail.com>
2022-08-31 23:54:22 +0900

    feat : 양방향 연관관계 마무리 및 연관관계 맵핑 기초 추가

--- src/main/java/association/AssociationApplication.java
+++ src/main/java/association/AssociationApplication.java
@@ -26,9 +26,13 @@ public class AssociationApplication {
             Mate mate = new Mate();
             mate.setUserName("member1");
             // 정석적인 방법으로 객체에 연관관계를 직접 넣어준다. -> 보통 연관관계 편의 메서드를 정의한다.
-            mate.setTeam(team);
-            team.getMates().add(mate);
+            mate.changeTeam(team); // 하지 않으면, 외래키가 NULL이 된다.. -> 연관관계 편의 메서드
+            // team.addMate(mate) -> 반대쪽에 연관관계 편의 메서드를 구현해도 된다. 상황을 보고 결정하자.
             em.persist(mate);
+
+            /* 연관관계 편의 매서드
+            team.getMates().add(mate);
+            */
 
             /* insert 쿼리가 발행되지 않았기 때문에, select 쿼리 또한 발행되지 않는다.
             *  따라서 flush를 실행하여 insert가 완료된 후, select 쿼리를 보낼 수 있도록 한다.
